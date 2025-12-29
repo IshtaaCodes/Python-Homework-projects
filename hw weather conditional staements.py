df00d830d4b89f4b9b0d949923255e63c8d05b51
IshtaaCodes <itsmeishtaa@gmail.com>
2025-12-29 17:09:26 +0530

    Add files via upload

---
+++ hw weather conditional staements.py
@@ -0,0 +1,5 @@
+temperature = float (input("enter the temperature in celcius"))
+if temperature>22 :
+    print("its Summer time wear light clothes")
+else :
+    print ("its winter time wear thick clothes")
